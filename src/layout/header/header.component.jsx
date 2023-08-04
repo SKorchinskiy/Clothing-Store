e89ac7495495170aca4d0de798e3f9e75aab5b00
SKorchinskiy <ddphyk@gmail.com>
2023-08-04 23:20:23 +0300

    + add interactivity for logo

--- src/layout/header/header.component.jsx
+++ src/layout/header/header.component.jsx
@@ -1,14 +1,16 @@
 import "./header.styles.scss";
 
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 import { ReactComponent as Logo } from "../../assets/crown.svg";
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
     <div className="header-container">
       <div className="header-container-side header-container-left">
-        <Logo />
+        <Logo onClick={() => navigate("/home")} style={{ cursor: "pointer" }} />
       </div>
       <div className="header-container-side  header-container-right">
         <Link to={"/shop"} className="header-container-element">
