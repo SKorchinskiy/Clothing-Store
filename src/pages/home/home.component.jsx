9abb340ec063cb68095c03c758ea473fabe3b953
SKorchinskiy <ddphyk@gmail.com>
2023-08-04 19:48:00 +0300

    + add home page

---
+++ src/pages/home/home.component.jsx
@@ -0,0 +1,39 @@
+import CategoryList from "../../components/category-list/category-list.component";
+
+const categories = [
+  {
+    id: 1,
+    title: "hats",
+    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+  },
+  {
+    id: 2,
+    title: "jackets",
+    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+  },
+  {
+    id: 3,
+    title: "sneakers",
+    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+  },
+  {
+    id: 4,
+    title: "womens",
+    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+  },
+  {
+    id: 5,
+    title: "mens",
+    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+  },
+];
+
+function Home() {
+  return (
+    <div className="home-page">
+      <CategoryList categories={categories} />
+    </div>
+  );
+}
+
+export default Home;
