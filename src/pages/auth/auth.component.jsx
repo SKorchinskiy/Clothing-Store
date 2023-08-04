bb32dd017d5dd7291b1c367f5841392ee45aff2c
SKorchinskiy <ddphyk@gmail.com>
2023-08-04 23:21:29 +0300

    + update auth component

--- src/pages/auth/auth.component.jsx
+++ src/pages/auth/auth.component.jsx
@@ -1,3 +1,17 @@
-function Auth() {}
+import "./auth.styles.scss";
+
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+
+function Auth() {
+  return (
+    <div className="auth-container">
+      <div className="auth-body-container">
+        <SignInForm />
+        <SignUpForm />
+      </div>
+    </div>
+  );
+}
 
 export default Auth;
