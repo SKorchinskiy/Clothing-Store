3986b6b8a5198a081ebc587463e2d8a96244d821
SKorchinskiy <ddphyk@gmail.com>
2023-08-10 17:45:52 +0300

    + update category list component with styled component

--- src/components/category-list/category-list.component.jsx
+++ src/components/category-list/category-list.component.jsx
@@ -1,14 +1,14 @@
-import "./category-list.styles.scss";
+import { CategoryListContainer } from "./category-list.styles";
 
 import Category from "../category/category.component";
 
 function CategoryList({ categories }) {
   return (
-    <div className="category-list-container">
+    <CategoryListContainer>
       {categories.map((category) => (
         <Category key={category.id} category={category} />
       ))}
-    </div>
+    </CategoryListContainer>
   );
 }
 
