c08b527ed4445c24d747dc0843c84cee8469128f
SKorchinskiy <ddphyk@gmail.com>
2023-08-10 18:21:43 +0300

    + update auth page with styled components

--- src/pages/auth/auth.styles.jsx
+++ src/pages/auth/auth.styles.jsx
@@ -1,11 +1,13 @@
-.auth-container {
+import styled from "styled-components";
+
+export const AuthContainer = styled.div`
   display: flex;
   justify-content: center;
   margin-top: 50px;
-}
+`;
 
-.auth-body-container {
+export const AuthBody = styled.div`
   width: 70%;
   display: flex;
   justify-content: space-around;
-}
+`;
