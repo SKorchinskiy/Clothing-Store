ee871ccf19443e0e54610a0d241d702b390dbd9e
SKorchinskiy <ddphyk@gmail.com>
2023-08-12 20:46:47 +0300

    add usage of user data from redux store into app

--- src/index.js
+++ src/index.js
@@ -9,15 +9,11 @@ import store from "./redux/store/store";
 import { Provider } from "react-redux";
 import { RouterProvider } from "react-router-dom";
 
-import { UserProvider } from "./contexts/user.context";
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <UserProvider>
-        <RouterProvider router={router} />
-      </UserProvider>
+      <RouterProvider router={router} />
     </Provider>
   </React.StrictMode>
 );
