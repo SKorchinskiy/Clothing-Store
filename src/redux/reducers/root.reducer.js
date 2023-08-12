b7399c7c872f6f0b98367304f2eed2411e1b1f81
SKorchinskiy <ddphyk@gmail.com>
2023-08-12 20:46:04 +0300

    + add user reducer and merge with root reducer

--- src/redux/reducers/root.reducer.js
+++ src/redux/reducers/root.reducer.js
@@ -1,11 +1,15 @@
 import { combineReducers } from "redux";
 
+import { userReducer } from "./user/user.reducer";
 import { cartReducer } from "./cart/cart.reducer";
 import { categoriesReducer } from "./categories/categories.reducer";
 
 const rootReducer = combineReducers({
+  user: userReducer,
   cart: cartReducer,
   categories: categoriesReducer,
 });
+
+console.log(rootReducer);
 
 export default rootReducer;
