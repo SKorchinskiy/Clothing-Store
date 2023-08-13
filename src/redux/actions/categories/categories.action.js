f219ecb772e74a804d3d882391fd1e0f5fc579a0
SKorchinskiy <ddphyk@gmail.com>
2023-08-13 20:09:47 +0300

    + update categories storing and action types

--- src/redux/actions/categories/categories.action.js
+++ src/redux/actions/categories/categories.action.js
@@ -1,5 +1,6 @@
 import { createAction } from "../create-action.helper";
+import { CATEGORIES_ACTION_TYPES } from "./categories.type";
 
 export function setCategoriesAction(categories) {
-  return createAction("SET_CATEGORIES", categories);
+  return createAction(CATEGORIES_ACTION_TYPES.SET_CATEGORIES, categories);
 }
