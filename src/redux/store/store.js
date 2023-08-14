9b78a1251b8407f171810de600a3436958a47701
SKorchinskiy <ddphyk@gmail.com>
2023-08-14 16:03:54 +0300

    + add persisted storage / persistGate

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,7 +1,16 @@
 import rootReducer from "../reducers/root.reducer";
 import { legacy_createStore as createStore } from "redux";
 import { devToolsEnhancer } from "@redux-devtools/extension";
+import { persistReducer, persistStore } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 
-const store = createStore(rootReducer, devToolsEnhancer());
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
 
-export default store;
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+export const store = createStore(persistedReducer, devToolsEnhancer());
+export const persistor = persistStore(store);
