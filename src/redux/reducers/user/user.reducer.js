c01358be0b5602c966025ead6afdabad89cb8ed3
SKorchinskiy <ddphyk@gmail.com>
2023-08-17 20:07:03 +0300

    + update user reducer to process new action types

--- src/redux/reducers/user/user.reducer.js
+++ src/redux/reducers/user/user.reducer.js
@@ -16,20 +16,27 @@ export function userReducer(state = INITIAL_USER_STATE, action) {
         currentUser: payload,
       };
     }
-    case USER_ACTION_TYPES.FETCH_CURRENT_USER_START: {
+    case USER_ACTION_TYPES.EMAIL_SIGN_IN_START:
+    case USER_ACTION_TYPES.GOOGLE_SIGN_IN_START:
+    case USER_ACTION_TYPES.EMAIL_SIGN_UP_START:
+    case USER_ACTION_TYPES.SIGN_OUT_START: {
       return {
         ...state,
         isLoading: true,
       };
     }
-    case USER_ACTION_TYPES.FETCH_CURRENT_USER_SUCCESS: {
+    case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
+    case USER_ACTION_TYPES.SIGN_UP_SUCCESS:
+    case USER_ACTION_TYPES.SIGN_OUT_SUCCESS: {
       return {
         ...state,
         isLoading: false,
         currentUser: payload,
       };
     }
-    case USER_ACTION_TYPES.FETCH_CURRENT_USER_FAILED: {
+    case USER_ACTION_TYPES.SIGN_IN_FAILED:
+    case USER_ACTION_TYPES.SIGN_UP_FAILED:
+    case USER_ACTION_TYPES.SIGN_OUT_FAILED: {
       return {
         ...state,
         isLoading: false,
