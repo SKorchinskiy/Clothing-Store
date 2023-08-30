ee8863d55a48bb8e536947fc696be512f36fbc9d
SKorchinskiy <ddphyk@gmail.com>
2023-08-30 14:08:08 +0300

    + add category component tests

---
+++ src/components/category/__tests__/category.component.test.js
@@ -0,0 +1,36 @@
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import { screen, render } from "@testing-library/react";
+
+import Category from "../category.component";
+
+import { category } from "../../../stubs/category.stub";
+
+const mockedUseNavigate = jest.fn();
+
+jest.mock("react-router-dom", () => {
+  const actualModule = jest.requireActual("react-router-dom");
+  return {
+    ...actualModule,
+    useNavigate: () => mockedUseNavigate,
+  };
+});
+
+describe("<Category /> component", () => {
+  it("should render UI elements properly", () => {
+    const { asFragment } = render(<Category category={category} />);
+
+    expect(asFragment()).toMatchSnapshot();
+  });
+
+  it("should navigate to category when clicked", () => {
+    render(<Category category={category} />);
+
+    const titleElement = screen.getByText(category.title);
+    userEvent.click(titleElement);
+
+    expect(mockedUseNavigate).toHaveBeenCalledWith(
+      `/category/${category.title}`
+    );
+  });
+});
---
+++ src/stubs/category.stub.js
@@ -0,0 +1,4 @@
+export const category = {
+  title: "test category",
+  imageUrl: "categoryImageUrl",
+};
