b3339be4ad1400bf9ff74dc0cf75fe290e96b67a
SKorchinskiy <ddphyk@gmail.com>
2023-08-31 14:07:41 +0300

    + move stubs to __tests__ folder inside appropriate components

---
+++ src/components/category-preview/__tests__/stubs/category-preview.stub.js
@@ -0,0 +1,23 @@
+export const categoryPreview = {
+  title: "category/title/1",
+  items: [
+    {
+      id: 1,
+      imageUrl: "category/1/item/imageUrl/1",
+      name: "category/1/item/name/1",
+      price: "category/1/item/price/1",
+    },
+    {
+      id: 2,
+      imageUrl: "category/1/item/imageUrl/2",
+      name: "category/1/item/name/2",
+      price: "category/1/item/price/2",
+    },
+    {
+      id: 3,
+      imageUrl: "category/1/item/imageUrl/3",
+      name: "category/1/item/name/3",
+      price: "category/1/item/price/3",
+    },
+  ],
+};
