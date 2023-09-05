3ed4994251c0c9770a28bf3fb5f868c7c5e81c18
SKorchinskiy <ddphyk@gmail.com>
2023-09-05 23:33:58 +0300

    + update category-list component to use typescript

--- src/components/category-list/category-list.component.tsx
+++ src/components/category-list/category-list.component.tsx
@@ -2,10 +2,20 @@ import { CategoryListContainer } from "./category-list.styles";
 
 import Category from "../category/category.component";
 
-function CategoryList({ categories }) {
+type CategoryType = {
+  id: number;
+  title: string;
+  imageUrl: string;
+};
+
+type CategoryListInputType = {
+  categories: Array<CategoryType>;
+};
+
+function CategoryList({ categories }: CategoryListInputType) {
   return (
     <CategoryListContainer>
-      {categories.map((category) => (
+      {categories.map((category: CategoryType) => (
         <Category key={category.id} category={category} />
       ))}
     </CategoryListContainer>
