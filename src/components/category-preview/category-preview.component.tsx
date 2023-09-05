394a8b809c02eeb20d900ec5cad123d7378cff8f
SKorchinskiy <ddphyk@gmail.com>
2023-09-05 23:34:12 +0300

    + update category-preview component to use typescript

--- src/components/category-preview/category-preview.component.tsx
+++ src/components/category-preview/category-preview.component.tsx
@@ -6,9 +6,21 @@ import {
 import { Fragment } from "react";
 import { useNavigate } from "react-router-dom";
 
-import ProductItem from "../product-item/product-item.component";
+import ProductItem, {
+  ProductType,
+} from "../product-item/product-item.component";
 
-function CategoryPreview({ category }) {
+type CategoryPreviewType = {
+  id: number;
+  title: string;
+  items: Array<ProductType>;
+};
+
+type CategoryPreviewInputType = {
+  category: CategoryPreviewType;
+};
+
+function CategoryPreview({ category }: CategoryPreviewInputType) {
   const { title, items } = category;
 
   const navigate = useNavigate();
